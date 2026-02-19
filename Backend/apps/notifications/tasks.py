741286f9954c18a02c8f5d5a519eacd8e23f5f1c
Muhammedshibil-123 <127181540+Muhammedshibil-123@users.noreply.github.com>
2026-02-19 22:44:19 +0530

    .env secrity issue fixed

--- Backend/apps/notifications/tasks.py
+++ Backend/apps/notifications/tasks.py
@@ -1,10 +1,9 @@
-
-import os
 import json
 import boto3
 from celery import shared_task
 from django.utils import timezone
 from django.contrib.auth import get_user_model
+from django.conf import settings
 from apps.tracking.models import DailyLog
 
 User = get_user_model()
@@ -13,23 +12,20 @@ User = get_user_model()
 def check_missing_meals(meal_type):
     today = timezone.now().date()
     
-    
     users_with_logs = DailyLog.objects.filter(
         date=today, 
         meal_type=meal_type
     ).values_list('user_id', flat=True)
     
-    
     users_without_logs = User.objects.exclude(id__in=users_with_logs).filter(is_active=True)
-    
     
     sqs = boto3.client(
         'sqs', 
-        region_name=os.getenv('AWS_REGION', 'ap-south-1'),
-        aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
-        aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY')
+        region_name=settings.AWS_REGION,
+        aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
+        aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY
     )
-    queue_url = os.getenv('SQS_QUEUE_URL')
+    queue_url = settings.AWS_MEAL_REMINDER_QUEUE_URL
     
     for user in users_without_logs:
         
