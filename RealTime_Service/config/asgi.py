c6144d8f7cdf528c8d3818911bb4b255bb1316a9
Muhammedshibil-123 <127181540+Muhammedshibil-123@users.noreply.github.com>
2026-02-27 16:53:28 +0530

    asgi update

--- RealTime_Service/config/asgi.py
+++ RealTime_Service/config/asgi.py
@@ -12,6 +12,7 @@ import django
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 django.setup()
 
